f9f6416aa1e21c32b965ba86b06369ae8cb914ae
Mahdiyar Delavari <MahdiyarDelavari@gmail.com>
2026-05-08 19:37:27 +0330

    Implement ws handling

--- backend/internal/routes/websockets.go
+++ backend/internal/routes/websockets.go
@@ -1,8 +1,67 @@
 package routes
 
-import ("backend/internal/realtime"
-"net/http")
+import (
+	"backend/internal/middlewares"
+	"backend/internal/models"
+	"backend/internal/realtime"
+	"backend/internal/utils"
+	"context"
+	"net/http"
+	"strings"
+
+	"github.com/coder/websocket"
+)
 
 func handleWebsocket(hub *realtime.Hub, w http.ResponseWriter, r *http.Request) {
+	authHeader := r.Header.Get(middlewares.CtxAuthorization)
+	if authHeader == "" || !strings.HasPrefix(strings.ToLower(authHeader), "bearer "){
+		utils.JSON(w, http.StatusUnauthorized, false, "Missing or invalid Authorization header", nil)
+		return
+	}
+	accessToken := strings.TrimSpace(authHeader[7:])
+
+	userId, _, _, err := utils.VerifyJWT(accessToken)
+
+	if err != nil {
+		utils.JSON(w, http.StatusUnauthorized, false, "Missing or invalid Authorization header", nil)
+		return
+	}
+
+	user, err := models.GetUserByID(userId)
+	if err != nil {
+		utils.JSON(w, http.StatusUnauthorized, false, "Missing or invalid Authorization header", nil)
+		return
+	}
+
+	opts := &websocket.AcceptOptions{
+		OriginPatterns: []string{""},
+	}
+
+	connection, err := websocket.Accept(w, r, opts)
+	if err != nil {
+		utils.JSON(w, http.StatusInternalServerError, false, "Failed to accept WebSocket connection", nil)
+		return
+	}
+
+	client:= realtime.NewClient(user, connection)
+
+	hub.RegisterClientConnection(client)
+	hub.SendCurrentClients(client)
+
+	defer func() {
+		hub.UnregisterClientConnection(client)
+		client.Close()
+	}()
+	
+	ctx, cancel := context.WithCancel(r.Context())
+	defer cancel()
+
+	go heartbeat(ctx, client)
+	go writePump(ctx, client)
+	readPump(ctx, cancel, hub , client)
+
+
+
+
 
 }
