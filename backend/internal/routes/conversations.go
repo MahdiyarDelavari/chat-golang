21f8fd6476e948ba8c0819df0a6f60ebb84495fc
Mahdiyar Delavari <MahdiyarDelavari@gmail.com>
2026-05-08 17:28:44 +0330

    Implement joinprivate

--- backend/internal/routes/conversations.go
+++ backend/internal/routes/conversations.go
@@ -1,8 +1,10 @@
 package routes
 
 import (
+	"backend/internal/middlewares"
 	"backend/internal/models"
 	"backend/internal/utils"
+	"encoding/json"
 	"net/http"
 	"strconv"
 )
@@ -23,4 +25,38 @@ func handlerGetPrivate(w http.ResponseWriter, r *http.Request) {
 
 	utils.JSON(w, http.StatusOK, true, "Private conversation fetched successfully", private)
 
+}
+
+func handlerJoinPrivate(w http.ResponseWriter, r *http.Request) {
+	userId := r.Context().Value(middlewares.CtxUserID).(int64)
+
+	var req struct {
+		RecieverID int64 `json:"receiver_id"`
+	}
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		utils.JSON(w, http.StatusBadRequest, false, "Invalid request body", nil)
+		return
+	}
+	if req.RecieverID == 0 {
+		utils.JSON(w, http.StatusBadRequest, false, "Receiver ID is required", nil)
+		return
+	}
+
+	private, err := models.GetPrivateByUsers(userId, req.RecieverID)
+	if err != nil {
+		utils.JSON(w, http.StatusInternalServerError, false, "Error occurred while fetching private conversation", nil)
+		return
+	}
+	if private == nil {
+		private, err = models.CreatePrivate(userId, req.RecieverID)
+		if err != nil {
+			utils.JSON(w, http.StatusInternalServerError, false, "Error occurred while creating private conversation", nil)
+			return
+		}
+		utils.JSON(w, http.StatusOK, true, "Joined private conversation successfully", private)
+		return
+	}
+
+	utils.JSON(w, http.StatusOK, true, "Joined private conversation successfully", private)
 }
