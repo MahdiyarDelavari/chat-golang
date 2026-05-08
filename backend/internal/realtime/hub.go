6d783b4f7f9481ee8d63d9ae5f1f23745fcf8018
Mahdiyar Delavari <MahdiyarDelavari@gmail.com>
2026-05-08 19:09:26 +0330

    fix send event to users and implement register client connection

--- backend/internal/realtime/hub.go
+++ backend/internal/realtime/hub.go
@@ -1,6 +1,7 @@
 package realtime
 
 import (
+	"backend/internal/models"
 	"log"
 	"sync"
 )
@@ -47,18 +48,69 @@ func (h *Hub) GetClients(userId int64) ([]*Client, bool) {
 	return clients, true
 }
 
-func (h *Hub) SendEventToUserIds(userIds []int64, eventType EventType, payload map[string]any) {
-		for _, userId := range userIds {
-			h.mu.RLock()
-			connections, ok := h.Clients[userId]
-			h.mu.RUnlock()
-			if ok {
-				for client := range connections {
-					client.SendEvent(Event{
-						EventType: eventType,
-						Payload:   payload,
-					})
-				}
-			}
+func (h *Hub) SendEventToUserIds(userIds []int64, sendId int64, eventType EventType, payload map[string]any) {
+	for _, id := range userIds {
+		h.mu.RLock()
+		conns, ok := h.Clients[id]
+		h.mu.RUnlock()
+
+		if !ok {
+			continue
 		}
+
+		for c := range conns {
+			c.SendEvent(Event{
+				EventType: eventType,
+				Payload:   payload,
+			})
+		}
+	}
+}
+
+func (h *Hub) RegisterClientConnection(client *Client) {
+	h.mu.Lock()
+	connections, ok := h.Clients[client.User.ID]
+	if !ok {
+		connections = make(map[*Client]struct{})
+		h.Clients[client.User.ID] = connections
+	}
+	connections[client] = struct{}{}
+	firstConnection := len(connections) == 1
+	h.mu.Unlock()
+
+	if firstConnection {
+		h.broadcastToAll(Event{
+			EventType: EventUserOnline,
+			Payload: map[string]any{
+				"user_id": client.User.ID,
+				"name":    client.User.Name,
+				"email":   client.User.Email,
+			},
+		})
+
+		go func() {
+			privates, err := models.GetPrivatesForUser(client.User.ID)
+			if err != nil {
+				log.Printf("Error fetching privates for user %d: %v", client.User.ID, err)
+				return
+			}
+			for _, private := range privates {
+					msgs, err := models.GetUndeliveredMessagesByPrivateID(private.ID)
+					if err != nil {
+						log.Printf("Error fetching undelivered messages for private %d: %v", private.ID, err)
+						continue
+					}
+					for _, msg := range msgs {
+						if msg.FromID == client.User.ID {
+							continue
+						}
+						h.SendEventToUserIds([]int64{msg.FromID}, client.User.ID,EventDelivered , map[string]any{
+							"message_id": msg.ID,
+							"to_id": client.User.ID,
+						})
+					}
+
+			}
+		}()
+	}
 }
